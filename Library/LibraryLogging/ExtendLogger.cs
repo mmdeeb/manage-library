7cc18786f05e44942a1e79f0f640a19c4abf8214
mohammed <mohammedyosefdeeb@gmail.com>
2022-09-26 16:53:13 +0300

    add all fils

---
+++ Library/LibraryLogging/ExtendLogger.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LibraryLogging
+{
+    public static class ExtendLogger
+    {
+        public static string FullName = "";
+        public static void LogOpration(this ILogger logger, string message)
+        {
+            logger.Log(FullName, message);
+        }
+    }
+}
---
+++ Library/LibraryLogging/Logger.cs
@@ -0,0 +1,34 @@
+﻿namespace LibraryLogging
+{
+    public sealed class Logger : ILogger
+    {
+        private static int InstanceCounter = 0;
+        private static Logger LoggerInstance = null;
+        public Logger() {
+            InstanceCounter++;
+        }
+      
+        public static Logger SingleInstance
+        {
+            get
+            {
+                if (LoggerInstance == null)
+                {
+                   LoggerInstance = new Logger();
+                }
+                return LoggerInstance;
+            }
+        }
+        public void Log(string message)
+        {
+            StreamWriter sw = File.AppendText("Logger.txt");
+            sw.WriteLine(message);
+            sw.Close();
+        }
+        public void Log(string FullName, string message)
+        {
+            Log($"{(FullName == "" ? "Manager" : FullName)} : {DateTime.Now} : {message}");
+        }
+    
+    }
+}
